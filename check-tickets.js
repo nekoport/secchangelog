8ddf9e1af5bf0c5f412aac0e78b459489df00bb4
nekoport <nekoport@users.noreply.github.com>
2026-08-10 15:28:35 +0700

    Remove temp debug scripts

--- check-tickets.js
+++
@@ -1,5 +0,0 @@
-const fs = require("fs");
-const s = fs.readFileSync("/app/data/secchangelog.db", "utf8");
-const re = /SOC-\d{4}\/\d{2}\/\d{2}-\d{4}/g;
-const m = s.match(re);
-console.log(m ? m.slice(0, 5) : "NO TICKET");
